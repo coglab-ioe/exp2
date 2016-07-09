45f4de66cd2d2d1990258246733e8c9e0e8db836
coglab-ioe <coglab.ioe@gmail.com>
2016-07-09 02:11:19 -0400

    Update tasks.js

--- task/tasks.js
+++ task/tasks.js
@@ -1,6 +1,6 @@
 //////////////// Task 1_ : one-touch (outside)
 canvas_span = 1.0; // 1.0 means the canvas being 100% width
-DEFAULT_RADIUS = 20;
+DEFAULT_RADIUS = 30;
 DEFAULT_COUNTDOWN = 0;
 TOUCH_MOVE_TIMEOUT = 200;
 
